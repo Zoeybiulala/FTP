0ea916191a3473313de28a6fa0caad9105f4f172
Zoeyabiubiu <1064271634@qq.com>
2023-04-08 14:05:58 -0400

    ads

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
