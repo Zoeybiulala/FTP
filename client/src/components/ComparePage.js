7ba098ed9eeea68ed0d46573e84f4fe1620619b1
Zoeyabiubiu <1064271634@qq.com>
2023-04-08 13:45:35 -0400

    comparison page

--- client/src/components/ComparePage.js
+++ client/src/components/ComparePage.js
@@ -1,35 +1,50 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import PageNavbar from "./PageNavbar";
 import Dropdown from "./Dropdown";
+import ComparisonTable from "./ComparisonTable";
 
-const states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
+const allStates = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"];
+const allMonths = Array.from({ length: 12 }, (_, i) => (i + 1).toString());
 
-export default function ComparePage(props) {
-    const [state1, setState1] = useState(states[0]);
-    const [state2, setState2] = useState(states[1]);
 
-    function handleState1Select(value) {
-        setState1(value);
+export default function ComparePage() {
+    const [selectedStates, setSelectedStates] = useState([]);
+    const [selectedMonth, setSelectedMonth] = useState('3');
+
+    function handleSelectState(state) {
+        // Add selected state to the array of selected states
+        setSelectedStates([...selectedStates, state]);
     }
 
-    function handleState2Select(value) {
-        setState2(value);
+    function handleSelectMonth(month) {
+        setSelectedMonth(month);
     }
 
+    function handleRemoveState(state) {
+        // Remove deselected state from the array of selected states
+        setSelectedStates(selectedStates.filter(s => s !== state));
+    }
     return (
         <div className="Compare-page">
             <PageNavbar active="Comparison" />
             <div className="container recommendations-container">
                 <br></br>
                 <div className="jumbotron findFriend-headspace">
-                    <div className="h5">Choose two states to compare:</div>
-                    <div style={{ display: "flex", justifyContent: "center", margin: "20px 0" }}>
-                        <Dropdown options={states} onSelect={handleState1Select} />
-                        <div style={{ margin: "0 10px" }}>vs.</div>
-                        <Dropdown options={states} onSelect={handleState2Select} />
+                    <em>Choose the states you want to compare:     </em>
+                    {/*<br></br>*/}
+                    <Dropdown options={allStates} onSelect={handleSelectState} />
+                    <em>     Choose the month:  </em>
+                    <Dropdown options={allMonths} onSelect={handleSelectMonth} />
+                    <br></br><br></br>
+                    <div>
+                        {selectedStates.map(state => (
+                            <button key={state} onClick={() => handleRemoveState(state)}>
+                                {state} &times;
+                            </button>
+                        ))}
                     </div>
-                    <div>State 1: {state1}</div>
-                    <div>State 2: {state2}</div>
+                    <ComparisonTable selectedStates={selectedStates} selectedMonth={selectedMonth} />
+
                 </div>
             </div>
         </div>
