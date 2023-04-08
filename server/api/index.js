426697449e15bc459fff8bc2009aefba17d4e976
Zoeyabiubiu <1064271634@qq.com>
2023-04-08 14:26:00 -0400

    routes!

--- server/api/index.js
+++ server/api/index.js
@@ -1,16 +1,8 @@
+const routes = require("../routes");
 const app = require('express')();
-const { v4 } = require('uuid');
 
-app.get('/api', (req, res) => {
-    const path = `/api/item/${v4()}`;
-    res.setHeader('Content-Type', 'text/html');
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
-
-app.get('/api/item/:slug', (req, res) => {
-    const { slug } = req.params;
-    res.end(`Item: ${slug}`);
-});
+app.get('/covid/confirm', routes.getTotalCase);
+app.get('/covid/death', routes.getTotalDeath);
+app.get('/fly', routes.getFlightInfo);
 
 module.exports = app;
